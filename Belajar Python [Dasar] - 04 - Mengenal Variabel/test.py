7529cf02cb086991015f7804f4c17b626b36008b
Iqbal Ramadhan Anniswa <95537097+kayabaakihiko13@users.noreply.github.com>
2021-12-24 23:12:18 +0700

    Add files via upload

---
+++ Belajar Python [Dasar] - 04 - Mengenal Variabel/test.py
@@ -0,0 +1,35 @@
+#Variabel adalah tempat menyimpan nilai
+##menyimpan Data
+a=10
+b=12
+z=20
+
+#pemanggilan varibel
+print("Nilai a adalah",a) #bisa mennggunakan model ini -->model ini kita  gunakan kedepannya
+print("Nilai b adalah",b)
+print("nilai z =",z)
+#penamaan varibel 
+"""====aturan penamaan variabel====
+1.boleh menggunakan nama panjang
+2.tidak boleh ada spasi,tapi menggunakan underscore
+3.tidak pakai -
+4.tidak boleh angka didepan
+"""
+#menggunakan nama panjang
+panjang=100000
+angka=200
+ribuan=4000
+
+print("Nilai panjang =",panjang)
+print("Nilai angka",angka)
+print("nilai ribuaan",ribuan)
+#contoh lainnya
+Nilai_rupiah=20000
+print("Nilai angka",angka)
+NilaiMean=2.54
+print("nilai ribuaan",ribuan)
+
+#enaknya python nama variabel sama tapi beda nilai masih bisa jalan !!!!
+z=9.23
+print("nilai z =",z)
+
