796bff4b7e0331ba8eb8ba1806c20ce622602d93
Iqbal Ramadhan Anniswa <95537097+kayabaakihiko13@users.noreply.github.com>
2021-12-24 16:56:17 +0700

    Add files via upload

---
+++ Belajar Python [Dasar] - 03 - Cara Kerja Program di python/main.py
@@ -0,0 +1,15 @@
+print("Apa Kabar My Friend")
+print("Hello World")
+print("hello World ")
+print("hello Cantkkkkk")
+
+#ini commnet
+a=10 #ini adalah comment
+""" Ada Apa dengan ucup dan Otong si ganteng
+dalam comment multiline
+"""
+print(a)
+
+# kita bisa mengcompile python ke namanya Bytecode
+
+
