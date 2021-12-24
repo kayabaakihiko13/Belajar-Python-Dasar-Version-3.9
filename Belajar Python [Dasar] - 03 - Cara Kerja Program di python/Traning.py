8843df1ac1dd96b43761aca112b47c192d9150ec
Iqbal Ramadhan Anniswa <95537097+kayabaakihiko13@users.noreply.github.com>
2021-12-24 17:12:04 +0700

    Add files via upload

---
+++ Belajar Python [Dasar] - 03 - Cara Kerja Program di python/Traning.py
@@ -0,0 +1,12 @@
+"""cara kerja Bahasa Programan"""
+
+print("hello Qadir-san")#cara mengularkan kaliamt "hello Qadir-san"
+print("Bagaimana Kabar Qadir-san")
+
+a=20 #ini bermaksud variabel a menyimpan nilai sebesar 20
+print(a)#berfungsi mengualarkan output variabel a tersebut
+
+""" ini adalah comment multi line"""
+#kita bisa mengcompile python lhoooooo
+#dengan cara buka terminal (windows buka command prompt)
+#
