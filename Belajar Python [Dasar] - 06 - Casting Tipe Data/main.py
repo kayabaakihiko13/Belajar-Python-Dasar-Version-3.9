1681c87600e4aba4e6e1fbb71a236e7cdb4f099c
Iqbal Ramadhan Anniswa <95537097+kayabaakihiko13@users.noreply.github.com>
2022-01-08 19:09:55 +0700

    Add files via upload

---
+++ Belajar Python [Dasar] - 06 - Casting Tipe Data/main.py
@@ -0,0 +1,63 @@
+# kita belajar casting
+# merubah dari sartu tipe ke tipe lain
+# tipe data = int,float,str,bool
+
+##Data Integer
+print("====Integer====")
+data_int=0
+print("data =",data_int,"tipe =",type(data_int))
+
+data_float=float(data_int)
+print("data =",data_float,"tipe =",type(data_float))
+
+data_str=str(data_int)
+print("data =",data_str,"tipe =",type(data_str))
+
+data_bool=bool(data_int) #akan False apabila nilai int =0
+print("data =",data_bool,"tipe =",type(data_bool))
+
+##Data Flaot
+
+print("====Float====")
+data_float=9.9
+print("data =",data_float,"tipe =",type(data_float))
+
+data_int=int(data_float)
+print("data =",data_int,"tipe =",type(data_int))
+
+data_str=str(data_float)
+print("data =",data_str,"tipe =",type(data_str))
+
+data_bool=bool(data_float) #akan False apabila nilai int =0
+print("data =",data_bool,"tipe =",type(data_bool))
+
+##Data Boolean
+
+print("====Boolean====")
+data_boolean=False
+print("data =",data_boolean,"tipe =",type(data_boolean))
+
+data_int=int(data_boolean)
+print("data =",data_int,"tipe =",type(data_int))
+
+data_str=str(data_boolean)
+print("data =",data_str,"tipe =",type(data_str))
+
+data_float=float(data_boolean) 
+print("data =",data_float,"tipe =",type(data_float))
+
+##Data String
+
+print("====String====")
+data_str=""
+print("data =",data_str,"tipe =",type(data_str))
+
+#data_int=int(data_str)
+#print("data =",data_int,"tipe =",type(data_int))
+
+data_boolean=bool(data_str)
+print("data =",data_boolean,"tipe =",type(data_boolean))
+
+#data_float=float(data_str) 
+#print("data =",data_float,"tipe =",type(data_float))
+
---
+++ Belajar Python [Dasar] - 07 - Mengambil Input Data dari User/main.py
@@ -0,0 +1,14 @@
+# Episode input user
+
+# data yang 
+data =input ("Masukan Nama: ")
+print("data =",data,"- tipe",type(data))
+
+#jika kita ingin mengambil integer
+angka =float(input("masukan Angka ="))
+angka =int(input("masukan Angka ="))
+print("data =",angka,"- tipe",type(angka))
+
+# bagaimana dengan boolean 
+biner=bool(int(input("masukan nilai boolean :")))
+print("data =",biner,"- tipe",type(biner))
