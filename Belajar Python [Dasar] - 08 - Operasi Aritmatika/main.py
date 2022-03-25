e6fc245ae6a40e05f32fd78e7b81e9f93753d736
Iqbal Ramadhan Anniswa <95537097+kayabaakihiko13@users.noreply.github.com>
2022-03-25 23:54:11 +0700

    Add files via upload

---
+++ Belajar Python [Dasar] - 08 - Operasi Aritmatika/main.py
@@ -0,0 +1,42 @@
+# operasi aritmatika 
+a =10 
+b =3
+
+# operasi tambah
+hasil=a+b
+print(a,"+",b,"=",hasil)
+
+# operasi pengurangan
+hasil=a-b
+print(a,"-",b,"=",hasil)
+
+# operasi kali (*)
+hasil=a*b
+print(a,"*",b,"=",hasil)
+
+# operasi pembagian
+hasil=a/b
+print(a,"/",b,"=",hasil)
+
+# operasi exponen(pangkat)
+hasil=a**b
+print(a,"**",b,"=",hasil)
+
+# operasi modulus
+hasil=a%b
+print(a,"%",b,"=",hasil)
+
+# operati florr
+hasil= a//b
+print(a,'//',b,'=',hasil)
+
+# prioritas operas,operational predence
+x=3
+y=2
+z=4
+
+hasil= x**y *z+x/y-y%z//x
+print(x,'**',y,'*',z,'+',x,'/',y,'-',y,'%',z,'//',x,"=",hasil)
+hasil= x+y*z
+print(x,'+',y,'*',z,'=',hasil)
+
